a18f8dd8493be50306c230a532a53d9145a93697
Jieun <jeeneep@naver.com>
2026-01-07 08:46:34 +0900

    [Week1] BOJ_10828_스택

---
+++ jeeneep/Boj10828.java
@@ -0,0 +1,38 @@
+package jeeneep;
+
+import java.io.*;
+import java.util.*;
+
+public class Boj10828 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder(); 
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+
+            switch (command) {
+                case "push":
+                    stack.addFirst(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    sb.append(stack.isEmpty() ? -1 : stack.removeFirst()).append("\n");
+                    break;
+                case "size":
+                    sb.append(stack.size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(stack.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case "top":
+                    sb.append(stack.isEmpty() ? -1 : stack.peekFirst()).append("\n");
+                    break;
+            }
+        }
+        System.out.print(sb);
+    }
+}
