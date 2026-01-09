94da51d66e56a4533d62e386294602240ae99c40
noiskk <zionzion00@naver.com>
2026-01-09 17:26:12 +0900

    week1 1월 9일

--- week01/noiskk/1158번-요세푸스 문제/Main.java
+++ week01/noiskk/1158번-요세푸스 문제/Main.java
@@ -9,3 +9,46 @@
 /*   Solved: 2026/01/09 08:17:03 by zionzion00    ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
+
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        // 입력 준비
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // 출력 준비
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+
+        // 자료구조 정의
+        Deque<Integer> deque = new ArrayDeque();
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        // deque 에 숫자 N개 넣기
+        for (int i = 1; i <= N; i++) {
+            deque.offer(i);
+        }
+        
+        while(!deque.isEmpty()){
+            // K-1만큼 뺐다가 다시 삽입
+            for(int i = 0; i < K - 1; i++){
+                deque.offer(deque.poll());
+            }
+            // K번째 수 출력
+            sb.append(deque.poll()).append(", ");
+        }
+
+        // 마지막 ", " 지우기
+        sb.delete(sb.length() - 2, sb.length());
+
+        // 출력 형식 완성
+        sb.append(">");
+
+        System.out.println(sb);
+    }
+}
