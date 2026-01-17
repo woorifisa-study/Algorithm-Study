d7313a02c2c6447c1d0e8df04a0d5c7c29bb5608
Jieun <jeeneep@naver.com>
2026-01-17 13:54:03 +0900

    [Week2] BOJ_1927_최소힙

---
+++ week02/jeeneep/BOJ_1927.java
@@ -0,0 +1,30 @@
+package week02.jeeneep;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_1927 {
+    public static void main(String[] args) throws IOException{
+        
+        // N 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x == 0) {
+                if (pq.isEmpty()) sb.append(0).append("\n");
+                else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            } 
+            else {
+                pq.offer(x);
+            }
+        }
+        System.out.println(sb);
+    }
+}
