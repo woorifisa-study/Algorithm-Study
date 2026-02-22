125a88cb23bd4e791a1756ff3c11289d0bd80778
Jieun <jeeneep@naver.com>
2026-02-22 18:22:47 +0900

    [Week6] BOJ_4796_캠핑

---
+++ week06/jeeneep/BOJ_4796.java
@@ -0,0 +1,33 @@
+package week06.jeeneep;
+
+import java.io.*;
+import java.util.*;
+
+// 4796
+public class BOJ_4796 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int L, P, V, result, remain;
+        int count = 0;
+        while (true) {
+            count++;
+            st = new StringTokenizer(br.readLine());
+            L = Integer.parseInt(st.nextToken());
+            P = Integer.parseInt(st.nextToken());
+            V = Integer.parseInt(st.nextToken());
+
+            if (L == 0 && P == 0 && V == 0) break;
+    
+            result = (V / P * L) + Math.min(V % P, L);
+
+            sb.append("Case " + count + ": " + result + "\n");
+
+        }
+        System.out.println(sb);
+    }
+    
+}
