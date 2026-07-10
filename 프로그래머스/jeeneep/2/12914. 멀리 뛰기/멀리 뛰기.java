3173e002dbb7e0d359a48b9b1f101a7a5acde916
Jieun Park <jeeneep@naver.com>
2026-07-10 13:56:48 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.07 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/jeeneep/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        
+        if (n == 1) return 1;
+        
+        int[] dp = new int[n + 1];
+        
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
+        }
+        
+        return dp[n];
+    }
+}
