437e824b33909f9f1ee0265c81befbed490e3f91
Jieun Park <jeeneep@naver.com>
2026-08-14 10:11:24 +0900

    [level 3] Title: 순위, Time: 8.71 ms, Memory: 89.2 MB -BaekjoonHub

---
+++ 프로그래머스/jeeneep/3/49191. 순위/순위.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int solution(int n, int[][] results) {
+        
+        int[][] floyd = new int[n + 1][n + 1];
+        
+        for (int[] result : results) {
+            int a = result[0];
+            int b = result[1];
+            
+            floyd[a][b] = 1;
+            floyd[b][a] = -1;
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= n; k++) {
+                    if (floyd[i][k] == 1 && floyd[k][j] == 1) {
+                        floyd[i][j] = 1;
+                        floyd[j][i] = -1;
+                    }
+                    if (floyd[i][k] == -1 && floyd[k][j] == -1) {
+                        floyd[i][j] = -1;
+                        floyd[j][i] = 1;
+                    }
+                }
+            }
+        }
+        
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            
+            int count = 0;
+            for (int j = 1; j <= n; j++) {
+                if (floyd[i][j] != 0) count++;
+            }
+            
+            if (count >= n - 1) answer ++;
+        }
+        
+        return answer;
+    }
+}
