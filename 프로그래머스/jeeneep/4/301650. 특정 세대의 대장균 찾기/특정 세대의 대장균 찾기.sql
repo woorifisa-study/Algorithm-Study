a49ed2ec92f6f8e77c4b55bb9b2cda3710a9d818
Jieun Park <jeeneep@naver.com>
2026-08-17 11:59:01 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/jeeneep/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/jeeneep/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,12 +1,23 @@
-select ID
-from ECOLI_DATA
-where PARENT_ID in (
-    select ID
+with recursive GENERATION_DATA as (
+    select 
+        ID,
+        PARENT_ID,
+        1 as GENERATION
     from ECOLI_DATA
-    where PARENT_ID in (
-        select ID
-        from ECOLI_DATA
-        where PARENT_ID is null
-    )
+    where PARENT_ID is null
+    
+    union all
+    
+    select 
+        CHILD.ID,
+        CHILD.PARENT_ID,
+        PARENT.GENERATION + 1 as GENERATION
+    from ECOLI_DATA CHILD
+    JOIN GENERATION_DATA PARENT
+        on CHILD.PARENT_ID = PARENT.ID   
 )
+
+select ID
+from GENERATION_DATA
+where GENERATION = 3
 order by ID asc;
