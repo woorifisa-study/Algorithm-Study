71802882ad3c58055716813e10ab16d70845fa54
noiskk <zionzion00@naver.com>
2026-08-21 14:40:28 +0900

    [level 3] Title: 없어진 기록 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/noiskk/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -0,0 +1,14 @@
+-- 입양을 간 기록은 있는데, 보호소에 들어온 기록이 없는 동물의 ID와 이름을 ID 순으로 조회하는 SQL문을 작성해주세요.
+
+SELECT
+    OUTS.ANIMAL_ID, OUTS.NAME
+FROM
+    ANIMAL_INS AS INS
+RIGHT JOIN
+    ANIMAL_OUTS AS OUTS
+ON
+    INS.ANIMAL_ID = OUTS.ANIMAL_ID
+WHERE
+    INS.ANIMAL_ID IS NULL
+ORDER BY
+    OUTS.ANIMAL_ID;
