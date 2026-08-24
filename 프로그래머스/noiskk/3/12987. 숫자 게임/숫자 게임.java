1eb007637132fffacadde728312ba2c87f4ce3f3
noiskk <zionzion00@naver.com>
2026-08-24 15:33:36 +0900

    [level 3] Title: 숫자 게임, Time: 41.46 ms, Memory: 69.7 MB -BaekjoonHub

---
+++ 프로그래머스/noiskk/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        int index = B.length - 1;
+        
+        for(int i = A.length - 1; i >= 0; i--){
+            if(A[i] < B[index]){
+                answer++;
+                index--;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
